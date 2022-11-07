9b12317adbc9b0a12fca2b356af8d06b1daa3617
Fangting Ma <64255604+Erica233@users.noreply.github.com>
2022-11-07 03:39:02 +0000

    query

--- connectdb.py
+++ connectdb.py
@@ -6,13 +6,51 @@ credentials = service_account.Credentials.from_service_account_file(
 project_id = 'intro-ai-339107'
 client = bigquery.Client(credentials=credentials, project=project_id)
 
-# Perform a query.
-QUERY = (
-    'SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` '
-    'WHERE state = "TX" '
-    'LIMIT 100')
-query_job = client.query(QUERY)  # API request
-rows = query_job.result()  # Waits for query to finish
+def query_year_sales():
+    QUERY = ("""
+        select release_year, round(sum(sales), 2) as total_sales
+        from `game_sales.game_sales`
+        group by release_year
+        order by release_year desc;
+    """)
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+    print("query 1:")
+    print("total sales of video game in each year")
+    print("year, total sales")
+    for row in rows:
+        print(row.release_year, row.total_sales)
 
-for row in rows:
-    print(row.name)
+def query_genre_sales():
+    QUERY = ("""
+        select genre, round(sum(sales), 2) as total_sales
+        from `game_sales.game_sales`
+        group by genre
+        order by total_sales desc;
+    """)
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+    print("\nquery 2:")
+    print("total sales of video game of different genres")
+    print("genre, total sales")
+    for row in rows:
+        print(row.genre, row.total_sales)
+
+def query_region_sales():
+    QUERY = ("""
+        select region, round(sum(sales), 2) as total_sales
+        from `game_sales.game_sales`
+        group by region
+        order by total_sales desc;
+    """)
+    query_job = client.query(QUERY)  # API request
+    rows = query_job.result()  # Waits for query to finish
+    print("\nquery 3:")
+    print("total sales of video game in different regions")
+    print("region, total sales")
+    for row in rows:
+        print(row.region, row.total_sales)
+
+query_year_sales()
+query_genre_sales()
+query_region_sales()
